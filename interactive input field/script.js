265b36d4b517d6ecc49a703573574f971b36552c
Gopal Vishwakarma <gopals2117@gmail.com>
2023-07-06 09:23:24 +0530

    improving transitions

--- interactive input field/script.js
+++ interactive input field/script.js
@@ -2,7 +2,6 @@ const inputField = document.querySelector(".input-field");
 const inputLabel = document.querySelector(".input-label");
 
 inputField.addEventListener("focus", function (e) {
-  console.log("event focus");
   e.currentTarget.style.border = "2px solid green";
   inputLabel.style.top = "20%";
   inputLabel.style.fontSize = "0.8rem";
@@ -10,10 +9,10 @@ inputField.addEventListener("focus", function (e) {
 });
 
 inputField.addEventListener("blur", function (e) {
-  console.log("event blur");
   if (e.currentTarget.value === "") {
     inputLabel.style.top = "50%";
     inputLabel.style.fontSize = "1rem";
     inputLabel.style.opacity = 1;
   }
+  e.currentTarget.style.border = "none";
 });
